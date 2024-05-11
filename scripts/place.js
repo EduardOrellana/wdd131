3acae5e9dcfdb33de6aa10240d7d9ce86436c913
EduardOrellana <ore23006@byui.edu>
2024-05-11 13:11:17 -0600

    fixing errors

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+const currentYear = document.getElementById('currentyear');
+const lastModified = document.getElementById('modified');
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last modification: ${document.lastModified}`;
