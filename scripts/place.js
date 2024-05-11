1665e6ae98e7727ecba0614976541a4c20ae5ba0
EduardOrellana <ore23006@byui.edu>
2024-05-11 14:36:36 -0600

    color font fixed

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,4 @@ const lastModified = document.getElementById('modified');
 
 currentYear.textContent = new Date().getFullYear();
 lastModified.textContent = `Last modification: ${document.lastModified}`;
+
