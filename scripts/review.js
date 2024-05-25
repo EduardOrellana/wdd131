7fb81be1eaaea9a68b47f93ad13f6515a6bc5f3c
EduardOrellana <ore23006@byui.edu>
2024-05-25 13:58:56 -0600

    adding a script with both files

--- scripts/review.js
+++ scripts/review.js
@@ -1,6 +1,7 @@
 //Counting and localStorage
 const textCount = document.getElementById("count");
-const buttonForm = document.querySelector('input[type="submit"');
+const buttonForm = document.querySelector('input[type="submit"]')
+
 let countLocalStorage = Number(window.localStorage.getItem("countingForm")) || 1;
 
 
@@ -8,11 +9,10 @@ let countLocalStorage = Number(window.localStorage.getItem("countingForm")) || 1
 function sumLocalStorage(){
     //This function will exucate each time the form was sent.
     countLocalStorage ++;
-    window.localStorage.setItem("countingForm");
+    window.localStorage.setItem("countingForm", countLocalStorage);
     textCount.textContent = `Thanks! this was your ${countLocalStorage} time`;
 }
 
-sumLocalStorage();
 
 //Footer
 const currentYear = document.getElementById('currentyear');
@@ -20,3 +20,7 @@ const lastModified = document.getElementById('modified');
 
 currentYear.textContent = new Date().getFullYear();
 lastModified.textContent = `Last modification: ${document.lastModified}`;
+
+//click into the button
+
+buttonForm.addEventListener("submit", sumLocalStorage);
